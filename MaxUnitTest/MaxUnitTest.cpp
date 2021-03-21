ee2ebe20cb8b743e3d54ed1ca324ac8db9371b79
Jakub Cimochowski <01158004@pw.edu.pl>
2021-03-21 17:15:00 +0100

    Added unit tests for thirdDigitSum() function

--- MaxUnitTest/MaxUnitTest.cpp
+++ MaxUnitTest/MaxUnitTest.cpp
@@ -12,6 +12,16 @@ namespace MaxUnitTest
 
 		TEST_METHOD(TestThirdDigitSum)
 		{
+			int sum;
+			sum = thirdDigitSum(247365);
+			Assert::AreEqual(sum, 5);
+			sum = thirdDigitSum(22);
+			Assert::AreEqual(sum, 0);
+			sum = thirdDigitSum(2212);
+			Assert::AreEqual(sum, 2);
+			sum = thirdDigitSum(5515111);
+			Assert::AreEqual(sum, 6);
 		}
+
 	};
 }
