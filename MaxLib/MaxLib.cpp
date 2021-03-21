ebbb02b9b860fa38bdfefaefe30159ff460a3686
Jakub Cimochowski <01158004@pw.edu.pl>
2021-03-21 17:29:16 +0100

    Added unit tests for max() function.

--- MaxLib/MaxLib.cpp
+++ MaxLib/MaxLib.cpp
@@ -1,5 +1,4 @@
 #include <array>
-#include <iostream>
 #include "Max.h"
 
 int max(std::array<std::array<int, maxColumns>, maxRows> array, size_t rows, size_t columns) {
--- MaxUnitTest/MaxUnitTest.cpp
+++ MaxUnitTest/MaxUnitTest.cpp
@@ -8,7 +8,7 @@ namespace MaxUnitTest
 	TEST_CLASS(MaxUnitTest)
 	{
 	public:
-		
+
 
 		TEST_METHOD(TestThirdDigitSum)
 		{
@@ -22,6 +22,18 @@ namespace MaxUnitTest
 			sum = thirdDigitSum(5515111);
 			Assert::AreEqual(sum, 6);
 		}
-
+		TEST_METHOD(TestMax1)
+		{
+			std::array<std::array<int, maxColumns>, maxRows> t = { {{1,2,3},{4,5,6},{7,8,9}} };
+			int maxElement = max(t, 3, 3);
+			Assert::AreEqual(maxElement, 0);
+		}
+		TEST_METHOD(TestMax2)
+		{
+			std::array<std::array<int, maxColumns>, maxRows> t = { {{121,2,223},{4,5,625144},{7,8,999}} };
+			int maxElement = max(t, 3, 3);
+			Assert::AreEqual(maxElement, 223);
+		}
 	};
 }
+
