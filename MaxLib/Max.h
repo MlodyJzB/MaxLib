76d57cbb125a80e7772836b54d6706a048e29911
Jakub Cimochowski <01158004@pw.edu.pl>
2021-03-21 17:11:56 +0100

    Funcion iterates each element in array; Added unit tests

---
+++ MaxLib/Max.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <array>
+
+const size_t maxColumns = 20;
+const size_t maxRows = 20;
+int max(std::array<std::array<int, maxColumns>, maxRows> array, size_t rows, size_t columns);
---
+++ MaxLib/MaxLib.cpp
@@ -0,0 +1,10 @@
+#include <array>
+#include "Max.h"
+
+int max(std::array<std::array<int, maxColumns>, maxRows> array, size_t rows, size_t columns) {
+	for (int c=0; c < int(columns); c++) {
+		for (int r = 0; r < int(rows); r++) {
+			array
+		}
+	}
+}
